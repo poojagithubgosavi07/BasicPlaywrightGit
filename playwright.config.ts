e8d72fd02b898e2b1932726b84050c7c7ce999e1
Pooja Gosavi <poojagosavi2526@gmail.com>
2026-04-15 20:39:08 +0530

    Initial Playwright framework commit 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
+  //reporter: [['html', { open: 'never' }]], //always
+    reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -34,10 +40,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-  //reporter: [['html', { open: 'always' }]],
-
   
-
   /* Configure projects for major browsers */
   projects: [
     {
